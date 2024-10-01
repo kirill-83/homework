c1e8a42432e43d505ea0398739839239c5e0cada
kirill-83 <kirill.pankov.83@mail.ru>
2024-10-01 14:56:44 +0700

    Add files via upload

---
+++ Переменные.py
@@ -0,0 +1,7 @@
+number_of_homeworks = int("12")
+print(number_of_homeworks)
+TimeSpent = float("1.5")
+print(TimeSpent)
+name = "Python"
+print(name)
+print(float(TimeSpent) / int(number_of_homeworks))
