51ccd5f6277c7cae9a96ec2f642d6e9dbbd0d214
kirill-83 <kirill.pankov.83@mail.ru>
2024-10-02 09:05:21 +0700

    Add files via upload

--- Переменные.py
+++ Переменные.py
@@ -1,7 +1,9 @@
-number_of_homeworks = int("12")
-print(number_of_homeworks)
-TimeSpent = float("1.5")
-print(TimeSpent)
-name = "Python"
-print(name)
-print(float(TimeSpent) / int(number_of_homeworks))
+first_name = "всего задач :"
+first_meaning = 12
+second_name = "затрачено часов :"
+second_meaning = 1.5
+name = "Курс: 'Python'"
+print(1.5 / 12)
+third_name = "среднее время выполнения"
+third_meaning = 0.125
+print(name , first_name, first_meaning,',', second_name, second_meaning,',',third_name, third_meaning)
